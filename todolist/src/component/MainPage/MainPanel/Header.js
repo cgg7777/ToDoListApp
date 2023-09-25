b9bd7ecade00adb88bed3037b9e816945d7e255a
Geomgi <cgg7777@naver.com>
2023-09-25 20:00:03 +0900

    날짜 이동 버튼 구현

--- todolist/src/component/MainPage/MainPanel/Header.js
+++ todolist/src/component/MainPage/MainPanel/Header.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import LeftArrow from "../icons/LeftArrow";
 import RightArrow from "../icons/RightArrow";
+import findDay from "../../../utils/findDay.js";
 const Header = () => {
+    const now = new Date();
+    const [fullDate, setFullDate] = useState(now);
+
+    const handleDownButtonClick = () => {
+        const yesterday = new Date(fullDate);
+        yesterday.setDate(fullDate.getDate() - 1);
+        setFullDate(yesterday);
+    };
+
+    const handleUpButtonClick = () => {
+        const tomorrow = new Date(fullDate);
+        tomorrow.setDate(fullDate.getDate() + 1);
+        setFullDate(tomorrow);
+    };
+
     return (
         <div style={{ display: "flex", justifyContent: "center", marginTop: "2vw" }}>
-            <div style={{ display: "flex", alignItems: "center", marginRight: "3vw" }}>
+            <div onClick={handleDownButtonClick} style={{ cursor: "pointer", display: "flex", alignItems: "center", marginRight: "3vw" }}>
                 <LeftArrow />
             </div>
             <div>
-                <p className="text">2023</p>
+                <p className="text">{fullDate.getFullYear()}</p>
                 <div style={{ alignItems: "flex-start" }}>
                     <p className="text" style={{ fontSize: "30px" }}>
-                        10.01
+                        {fullDate.getMonth() + 1}.{String(fullDate.getDate()).padStart(2, "0")}
                     </p>
                 </div>
-                <p className="text">SUN</p>
+                <p className="text">{findDay[fullDate.getDay()]}</p>
             </div>
-            <div style={{ display: "flex", alignItems: "center", marginLeft: "3vw" }}>
+            <div onClick={handleUpButtonClick} style={{ cursor: "pointer", display: "flex", alignItems: "center", marginLeft: "3vw" }}>
                 <RightArrow />
             </div>
         </div>
